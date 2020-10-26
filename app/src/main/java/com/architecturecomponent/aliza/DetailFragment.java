a6014da19aa2dfc2fd9f0016cadadd0168fd0176
alizarizqi <54764338+alizarizqi@users.noreply.github.com>
2020-10-26 19:37:44 +0700

    update fragment & add binding

--- app/src/main/java/com/architecturecomponent/aliza/DetailFragment.java
+++ app/src/main/java/com/architecturecomponent/aliza/DetailFragment.java
@@ -12,11 +12,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.architecturecomponent.aliza.databinding.DetailFragmentBinding;
+
 import com.architecturecomponent.aliza.viewModel.DetailViewModel;
 
 public class DetailFragment extends Fragment {
 
     private DetailViewModel mViewModel;
+    DetailFragmentBinding binding;
 
     public static DetailFragment newInstance() {
         return new DetailFragment();
@@ -25,14 +28,27 @@ public class DetailFragment extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.detail_fragment, container, false);
+        binding = DetailFragmentBinding.inflate(inflater, container, false);
+        View view = binding.getRoot();
+        binding.txtnama.setText(getArguments().getString("nama"));
+        binding.txtprovinsi.setText(getArguments().getString("jumlah"));
+        binding.txttelpRumah.setText(getArguments().getString("harga"));
+        binding.txthp.setText(getArguments().getString("total"));
+        binding.textView3.setText(getArguments().getString("metode"));
+        binding.txtbayar.setText(getArguments().getString("bayar"));
+        binding.textView10.setText(getArguments().getString("kembalian"));
+        binding.textView12.setText(getArguments().getString("bank"));
+
+
+
+        return view;
     }
 
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        mViewModel = ViewModelProviders.of(this).get(DetailViewModel.class);
-        // TODO: Use the ViewModel
-    }
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//        mViewModel = ViewModelProviders.of(this).get(DetailViewModel.class);
+//        // TODO: Use the ViewModel
+//    }
 
 }
--- app/src/main/java/com/architecturecomponent/aliza/InputFragment.java
+++ app/src/main/java/com/architecturecomponent/aliza/InputFragment.java
@@ -5,11 +5,13 @@ import android.os.Bundle;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProviders;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.architecturecomponent.aliza.databinding.FragmentInputBinding;
 import com.architecturecomponent.aliza.viewModel.DetailViewModel;
 import com.architecturecomponent.aliza.viewModel.viewModel;
 
@@ -17,17 +19,83 @@ import com.architecturecomponent.aliza.viewModel.viewModel;
 public class InputFragment extends Fragment {
 
 private viewModel vm;
+    FragmentInputBinding binding;
+    Bundle bundle;
+    private String jumlah;
+
+    public void setJumlah(String jumlah){
+        this.jumlah = jumlah;
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_input, container, false);
+        binding = FragmentInputBinding.inflate(inflater,container,false);
+        View view =binding.getRoot();
+
+        binding.btnTampil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                bundle = new Bundle();
+                bundle.putString("nama", binding.spinnerNama.getSelectedItem().toString());
+                if(binding.radioCash.isChecked()){
+                    bundle.putString("metode", "Cash");
+                }
+                else{
+                    bundle.putString("metode", "Debit");
+                }
+
+                bundle.putString("jumlah", binding.editJmlBrg.getText().toString());
+                bundle.putString("harga", binding.editHargaBrg.getText().toString());
+                bundle.putString("total", binding.txtTotal.getText().toString());
+                bundle.putString("bayar", binding.editBayar.getText().toString());
+                bundle.putString("kembalian", binding.txtKembalian.getText().toString());
+                bundle.putString("bank", binding.spinnerBank.getSelectedItem().toString());
+
+                Navigation.findNavController(view).navigate(R.id.action_inputFragment_to_detailFragment,bundle);
+            }
+        });
+
+        binding.btnTotal.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double hrgBrg;
+                double total;
+                double jmlBrg = Double.parseDouble(binding.editJmlBrg.getText().toString());
+                hrgBrg = Double.parseDouble(binding.editHargaBrg.getText().toString());
+                total = jmlBrg*hrgBrg;
+                binding.txtTotal.setText(String.valueOf(total));
+
+            }
+        });
+
+        binding.btnHasil.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                double bayar;
+                double tt;
+                bayar = Double.parseDouble(binding.editBayar.getText().toString());
+                tt = Double.parseDouble(binding.txtTotal.getText().toString());
+                double kembalian = bayar-tt;
+                binding.txtKembalian.setText(String.valueOf(kembalian));
+
+            }
+        });
+
+
+
+
+
+
+        return view;
     }
-    @Override
-    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        vm = ViewModelProviders.of(this).get(viewModel.class);
-        // TODO: Use the ViewModel
-    }
+
+//    @Override
+//    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+//        super.onActivityCreated(savedInstanceState);
+//        vm = ViewModelProviders.of(this).get(viewModel.class);
+//        // TODO: Use the ViewModel
+//
+//    }
 }
